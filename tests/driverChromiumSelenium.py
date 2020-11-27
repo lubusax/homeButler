a71587a6e21444032b98ac825342471951632cf6
lubusax <lu.bu.sax@gmail.com>
2020-11-27 22:42:28 +0000

    auto

--- tests/driverChromiumSelenium.py
+++ tests/driverChromiumSelenium.py
@@ -1,9 +1,28 @@
-from selenium import webdriver
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-browser = webdriver.Firefox(executable_path="/usr/lib/geckodriver")
-browser.get('https://www.linuxhint.com')
-print('Title: %s' % browser.title)
-browser.quit()
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+
+if __name__ == "__main__":
+    options = Options()
+    options.add_argument('-headless')
+    driver = Firefox(executable_path='/usr/local/bin/geckodriver', options=options)
+    wait = WebDriverWait(driver, timeout=10)
+    driver.get('http://www.google.com')
+    wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
+    wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
+    print(driver.page_source)
+    driver.quit()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# browser = webdriver.Firefox(executable_path="/usr/lib/geckodriver")
+# browser.get('https://www.linuxhint.com')
+# print('Title: %s' % browser.title)
+# browser.quit()
 
 
 
