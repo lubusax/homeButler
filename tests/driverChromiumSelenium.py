9ba6301da7f6d42ffbd06aaa507c1a23e871dc39
lubusax <lu.bu.sax@gmail.com>
2020-11-28 01:46:28 +0000

    auto

--- tests/driverChromiumSelenium.py
+++ tests/driverChromiumSelenium.py
@@ -1,49 +1,76 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as expected
-from selenium.webdriver.support.wait import WebDriverWait
+import sys
 
-if __name__ == "__main__":
-    options = Options()
-    options.add_argument('-headless')
-    driver = Firefox(executable_path='/usr/local/bin/geckodriver', options=options)
-    wait = WebDriverWait(driver, timeout=10)
-    driver.get('http://www.google.com')
-    wait.until(expected.visibility_of_element_located((By.NAME, 'q'))).send_keys('headless firefox' + Keys.ENTER)
-    wait.until(expected.visibility_of_element_located((By.CSS_SELECTOR, '#ires a'))).click()
-    print(driver.page_source)
-    driver.quit()
+# sys.path.append("/home/pi/ras/lib")
+# sys.path.append("/home/pi/ras/dicts")
+sys.path.append("/home/pi/ras")
+
+from lib import Display
+from lib import Utils
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+#@Utils.timer
+def measurementUpDownBandwidth(driver, measurementPeriod):
+  go_button = driver.find_element_by_css_selector(".start-button a")
+  go_button.click()
+  print("after click go measure")
+  #timestamp = time.strftime('%y%m%d%H%M%S',time.localtime())
+  timestamp = time.strftime('%d%H%M%S',time.localtime())
+  time.sleep(measurementPeriod)
+  #driver.get_screenshot_as_file("capture.png") # works fine
+  driver.save_screenshot("capture.png")
+  up = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+  down = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+  return [timestamp,float(up),float(down)]
+
+@Utils.timer
+def getDriver():
+  chrome_options = Options()
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--window-size=1920x1080")
+  chrome_options.add_argument("--no-sandbox")
+
+  driver = webdriver.Chrome(options=chrome_options, executable_path='/usr/local/share/chromedriver')  # Optional argument, if not specified will search path.
+  return driver
+
+@Utils.timer
+def getWebSite(driver, webSite):
+  driver.get(webSite)
+  time.sleep(5)
+
+@Utils.timer
+def clickAcceptGDPRpopup(driver):
+  # Depending on your location, you might need to accept the GDPR pop-up.
+  accept_button = driver.find_element_by_id("_evidon-banner-acceptbutton")
+  accept_button.click()
+  print("after click GDPR pop-up")
+  time.sleep(3)
+
+Disp = Display.Display()
+Disp.displayMsgRaw([(0,0), 16, "Hello"])
+driver = getDriver()
+webSite = "https://www.speedtest.net/"
+measurementPeriod = 60 # seconds
+minUp = 10000
+maxUp = 0
+getWebSite(driver, webSite)
+clickAcceptGDPRpopup(driver)
+while True:
+  [timestamp,up,down] = measurementUpDownBandwidth(driver, measurementPeriod)
+  print("after measurements")
+  print("TIMESTAMP: ", timestamp)
+  print("UP: ", up)
+  print("DOWN: ", down)
+  if up>maxUp:
+    maxUp =(up)
+
+  if up<minUp:
+    minUp=up
+    timeMin = timestamp
+
+  Disp.displayMsgRaw([(0,5), 16, "LAST: "+str(up)+ "/nMIN: "+str(minUp)+"/ntime MIN: "+ str(timeMin)+ "/nMAX: "+str(maxUp)])
 
 
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# browser = webdriver.Firefox(executable_path="/usr/lib/geckodriver")
-# browser.get('https://www.linuxhint.com')
-# print('Title: %s' % browser.title)
-# browser.quit()
-
-
-
-# from selenium import webdriver
-# # from selenium.webdriver.chrome.options import Options
-
-# # chromium_path = "/usr/lib/chromium-browser"
-
-# #chromedriver_path = "/usr/bin/chromedriver"
-
-# # opts = Options()
-# # opts.binary_location = chromium_path
-# opts.add_argument("headless")
-# # driver = webdriver.Chrome(chrome_options=opts)
-
-# #driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')  # Optional argument, if not specified will search path.
-
-# driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
-
-# driver.get('http://www.google.com/xhtml');
-# time.sleep(5) # Let the user actually see something!
-# someText = driver.find_element_by_name('SIvCob').text
-# print("SIvCob :", someText)
-# driver.quit()
