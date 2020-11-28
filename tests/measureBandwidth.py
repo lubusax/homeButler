ac0535f323f465a0fc736bbfd9bcd361fd116d03
lubusax <lu.bu.sax@gmail.com>
2020-11-28 22:18:31 +0000

    auto

--- tests/measureBandwidth.py
+++ tests/measureBandwidth.py
@@ -25,19 +25,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 
+def getTimestamp():
+  return time.strftime('%y%m%d%H%M%S',time.localtime())
+
 #@Utils.timer
 def measurementUpDownBandwidth(driver, measurementPeriod):
+  print("in method measurementUpDownBandwidth ")
   go_button = driver.find_element_by_css_selector(".start-button a")
   go_button.click()
-  print("after click go measure")
-  #timestamp = time.strftime('%y%m%d%H%M%S',time.localtime())
-  timestamp = time.strftime('%H%M',time.localtime())
+  timestamp = getTimestamp()
+  driver.save_screenshot("screenBefore.png")
   time.sleep(measurementPeriod)
-  #driver.get_screenshot_as_file("capture.png") # works fine
-  driver.save_screenshot("capture.png")
+  driver.save_screenshot("screenAfter.png")  
   up = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
   down = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
   return [timestamp,float(up),float(down)]
+
+def makeScreenshotMeasurement(timestampLastScreenshot, timeUntilNextScreenshot, periodBetweenScreenshots):
+  if timeUntilNextScreenshot < 0:
+    pathScreenshot = os.getcwd()+"/measurementsBandwidth/screenshotBandwidth" + getTimestamp() + ".png"
+    driver.save_screenshot(pathScreenshot)
+    timestampLastScreenshot = int(time.time())
+    timeUntilNextScreenshot = periodBetweenScreenshots
+  else:
+    timeUntilNextScreenshot = timeUntilNextScreenshot + timestampLastScreenshot - time.localtime()
+  return (timestampLastScreenshot, timeUntilNextScreenshot)
 
 @Utils.timer
 def getDriver():
@@ -47,6 +59,7 @@ def getDriver():
   options.add_argument("--no-sandbox")
 
   driver = webdriver.Chrome(options=options, executable_path='/usr/local/share/chromedriver')
+  return driver
 
 @Utils.timer
 def getWebSite(driver, webSite):
@@ -61,46 +74,53 @@ def clickAcceptGDPRpopup(driver):
   print("after click GDPR pop-up")
   time.sleep(3)
 
-def appendMeasurement(filePath,timestamp, up, down):
-  pass
+def appendMeasurement(pathFile,timestamp, up, down):
+  with open(pathFile, 'a') as f: 
+    f.write(str(timestamp)+","+str(up)+","+str(down)+"\n")
 
-measurementPeriod = 60 # seconds
+def getPathFileBandwidthMeasurements():
+  directory = os.getcwd()+"/measurementsBandwidth"
+  if not os.path.exists(directory):
+    os.mkdir(directory)
+  pathFile = directory + "/measurementsBandwidth" + getTimestamp()
+  with open(pathFile, 'w') as f: 
+    pass
+  return pathFile
+  
+measurementPeriod = 600 # seconds
 minUp = 10000
 maxUp = 0
-up=10.0
-timeMin = "0204"
-webSite = "https://www.speedtest.net/"
-fileBandwidthMeasurements = "fileBandwidthMeasurements"
+timeUntilNextScreenshot = -1 
+periodBetweenScreenshots = 3600 # seconds
+timestampLastScreenshot = 0
+webSite = "https://www.speedtest.net/de"
+
+fileBandwidthMeasurements = getPathFileBandwidthMeasurements()
 
 Disp = Display.Display()
-Disp.displayMsgRaw([(0,0), 16, "Hello"])
+#Disp.displayMsgRaw([(0,0), 16, "Hello"])
 
 driver = getDriver()
-# getWebSite(driver, webSite)
-
-# clickAcceptGDPRpopup(driver)
 
 while True:
   try:
-    Disp.displayMsgRaw([(0,1), 16, "LAST: "+str(up)+ "\nMIN: "+str(minUp)+"\ntime MIN: "+ str(timeMin)+ "\nMAX: "+str(maxUp)])
     getWebSite(driver, webSite)
+
     try:
       clickAcceptGDPRpopup(driver)
     except Exception as e:
       print ("there was an exception while trying to click GDPR Pop Up: ",e)
+
     [timestamp,up,down] = measurementUpDownBandwidth(driver, measurementPeriod)
+
+    if up>maxUp: maxUp = up
+
+    if up<minUp: (minUp, timeMin) = (up, timestamp)
+
+    (timestampLastScreenshot, timeUntilNextScreenshot) = makeScreenshotMeasurement(timestampLastScreenshot, timeUntilNextScreenshot, periodBetweenScreenshots)
     appendMeasurement(fileBandwidthMeasurements,timestamp, up, down)
-    print("after measurements")
-    print("TIMESTAMP: ", timestamp)
-    print("UP: ", up)
-    print("DOWN: ", down)
-    if up>maxUp:
-      maxUp =(up)
-
-    if up<minUp:
-      minUp=up
-      timeMin = timestamp
-
+    
+    print("after measurements "+timestamp+"- UP: "+str(up)+"- DOWN: "+str(down) )
     Disp.displayMsgRaw([(0,5), 16, "LAST: "+str(up)+ "/nMIN: "+str(minUp)+"/ntime MIN: "+ str(timeMin)+ "/nMAX: "+str(maxUp)])
   except Exception as e:
     print ("there was an exception while trying to measure the bandwidth: ",e)
